03c8a7ff6a9a4e80e3ddf29bfa3808ff54f90a69
botlax <lacsinapaul@gmail.com>
2015-10-05 00:54:58 +0300

    Update AuthController.php

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -33,7 +33,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest', ['except' => ['getLogout','getRegister']]);
+        $this->middleware('guest', ['except' => ['getLogout','getRegister','postRegister']]);
     }
     /**
      * Get a validator for an incoming registration request.
