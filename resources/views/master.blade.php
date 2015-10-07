ead80cc20b43ea2f9d9c45b497fc1c20ccb0d484
botlax <lacsinapaul@gmail.com>
2015-10-07 15:59:16 +0300

    Viewport value update

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,6 +4,7 @@
 
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="{{url('css/normalize.css')}}">
         <link rel="stylesheet" href="{{url('vendor/bootstrap/css/bootstrap.min.css')}}">
         <link rel="stylesheet" href="{{url('vendor/select2/select2.css')}}">
