137edd64fb15fa9ae15c286c5eef865ec04973ea
botlax <lacsinapaul@gmail.com>
2015-10-10 15:34:29 +0300

    Update AttendanceController.php

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -166,7 +166,7 @@ class AttendanceController extends Controller
                 $total[$labor->employee_no]['bot'] =  $bot_count;
                 
                 //total salary
-                $salary[$labor->employee_no]['attended'] = ((intval($labor->basic_salary) + intval($labor_allowance)) / intval($dEnd)) * $att_count;
+                $salary[$labor->employee_no]['attended'] = ((intval($labor->basic_salary) + intval($labor->allowance)) / intval($dEnd)) * $att_count;
                 $salary[$labor->employee_no]['ot'] = $ot_count;
                 $salary[$labor->employee_no]['bot'] =  $bot_count;
             }
