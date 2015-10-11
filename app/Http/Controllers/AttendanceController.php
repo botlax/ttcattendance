76d3ca33c78cd6b61d1344dbd36c9c97cc7ed818
botlax <lacsinapaul@gmail.com>
2015-10-11 09:35:45 +0300

    Update AttendanceController.php

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -31,6 +31,9 @@ class AttendanceController extends Controller
      */
     public function showFilterOptions()
     {
+        foreach(Labor::all() as $labor){
+            Attendance::where('id',5)->first()->Labor->attach($labor->id);
+        }
         $sites = Site::where('id','>',1)->get()->lists('code','id')->toArray();
         $months = ['1'=>'January','2'=>'February','3'=>'March','4'=>'April','5'=>'May','6'=>'June','7'=>'July','8'=>'August','9'=>'September','10'=>'October','11'=>'November','12'=>'December'];
         $years = [];
